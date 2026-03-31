2699c0bc30f760275681ec29bb84039128e69442
raymondnowater <raymond.nouata@outlook.com>
2026-03-31 17:35:50 +1100

    Implement roles in the model

--- main.py
+++ main.py
@@ -2,30 +2,31 @@ import os
 import argparse
 from dotenv import load_dotenv
 from google import genai
+from google.genai import types
 
-load_dotenv()
-api_key = os.environ.get("GEMINI_API_KEY")
 
-if api_key == None:
-    raise RuntimeError("api key not found")
-
-client = genai.Client(api_key=api_key)
-
-parser = argparse.ArgumentParser(description="Chatbot")
-parser.add_argument("user_prompt", type=str, help="User prompt")
-args = parser.parse_args()
 
 def main():
-    
-    print("Hello from aiagent!")
-    prompt=args.user_prompt
+    parser = argparse.ArgumentParser(description="Chatbot")
+    parser.add_argument("user_prompt", type=str, help="User prompt")
+    args = parser.parse_args()
+
+    load_dotenv()
+    api_key = os.environ.get("GEMINI_API_KEY")
+
+    if api_key == None:
+        raise RuntimeError("api key not found")
+
+    client = genai.Client(api_key=api_key)
+
+    messages = [types.Content(role="user", parts=[types.Part(text=args.user_prompt)])]
 
     response = client.models.generate_content(
-            model='gemini-2.5-flash', contents=prompt)
+            model='gemini-2.5-flash', contents=messages)
 
     if response.usage_metadata == None:
         raise RuntimeError("Metadata missing")
-    print("User prompt: ", prompt)
+    print("User prompt: ", args.user_prompt)
     print("Prompt tokens: ", response.usage_metadata.prompt_token_count) 
     print("Response tokens: ", response.usage_metadata.candidates_token_count)
     print("Response: ", response.text)
